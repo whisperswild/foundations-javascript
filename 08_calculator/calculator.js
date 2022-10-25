f3b63e29866a4faf355f4a0a20c9e4813e5c32d5
Chris <Whisperswild@gmail.com>
2022-10-24 21:53:31 -0500

    Complete assignment 08 and 09, calculator and palindromes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,59 @@
-const add = function() {
-	
+const add = function(a, b) {
+  let result = parseFloat(a + b);
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  let result = parseFloat(a - b);
+  return result;
 };
 
 const sum = function() {
-	
+	//add up any number of numbers
+
+  let result = 0;
+  let args = arguments[0];
+  for (a in args){
+    if(args.length>0){
+
+      result += args[a];
+    }else{
+      return 0;
+      break;
+    }
+  }
+  return result;
+
 };
 
 const multiply = function() {
+  let result = 1;
+  let nums = arguments[0]
+  
+    for(a in nums){
+      result *= parseFloat(nums[a]);
+    }
 
+    return result;
+  
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = parseFloat(a ** b);
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	//factorial multiplies the number by 0, 1, 2, 3, etc. So 4! would be 1*2*3*4
+  let i = 1;
+  let result = 1;
+  while (i<= a){
+    result *= i;
+    i++;
+  }
+
+  return result;
+
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (t) {
+//need to drop punctuation?
+    
+    const cleanText = t.replace(/\W/g, '')
+    const forward = Array.from(cleanText.toLowerCase());
+    
+   forward.reverse();
+    
 
+    forwardStr = forward.join('');
+    backwardStr = forward.reverse().join('');
+
+    console.log(`Forward: ${forwardStr}`);
+    console.log(`Backward: ${backwardStr}`); 
+
+    return (forwardStr===backwardStr ? true:false);
+
+   
 };
+
+function cleanStr(text){
+    return text.match(/[a-z]/i);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
