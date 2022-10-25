1ca2857792b3e1faaaa6b0ae4bb5a922d993b1e8
Chris <Whisperswild@gmail.com>
2022-10-25 00:27:50 -0500

    Complete assignment 11, array exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,26 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArray) {
+    const bookList = Array.from(bookArray);
+    const titles = new Array();
 
+    for (const book of bookList){
+        titles.push(book.title);
+    }
+
+    return titles;
 };
+
+/*
+const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ]
+*/
 
 // Do not edit below this line
 module.exports = getTheTitles;
