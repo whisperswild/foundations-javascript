56595deb860dbbbe16e331bf27c9d0106ea2377f
Chris <Whisperswild@gmail.com>
2022-10-25 15:25:11 -0500

    Complete the 'findTheOldest' assignment for sorting arrays

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+
+
+const ages = people.map(person => ({
+    ...person, age: person.yearOfDeath - person.yearOfBirth
+    }));
+
+for (let a in ages){
+    if (isNaN(ages[a].age)){
+        ages[a].age = new Date().getFullYear() - ages[a].yearOfBirth;
+    }
+}
+
+const oldest = ages.sort((a, b) => b.age - a.age);
+console.table(oldest);
+return oldest[0];
+
+
 
 };
 
